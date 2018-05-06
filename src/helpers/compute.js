82e6270acd1b3d56306a675d1c255adc21b979fb
tollmanz <tollmanz@gmail.com>
2018-05-06 10:11:42 -0500

    Add support for different points per round

--- src/helpers/compute.js
+++ src/helpers/compute.js
@@ -2,8 +2,34 @@ const _ = require('lodash');
 
 const data = require('./data');
 
-const POINTS_FOR_PICK = 1;
+const POINTS_FOR_PICK = {
+  1: 1,
+  2: 3,
+  3: 5,
+  4: 7
+};
 const POINTS_FOR_GAMES = 2;
+
+const getRoundFromGame = (game) => {
+  const mapping = {
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1,
+    5: 2,
+    6: 2,
+    7: 3,
+    8: 4
+  };
+
+  const gameNumber = game.slice(-1);
+  return mapping[gameNumber] || 0;
+};
+
+const getPointsForGame = (game) => {
+  const round = getRoundFromGame(game);
+  return POINTS_FOR_PICK[round];
+};
 
 const getCompetitorChoiceForMatchup = (competitor, choices, matchupName) => {
   const competitorChoices = _.find(choices, { competitor }).choices || {};
@@ -40,7 +66,7 @@ const computeCompetitorScore = (competitor, choices, matchups) => {
   return competitorResults.reduce((score, result) => {
     const { pick, games } = result.results;
     if (pick) {
-      score = score + POINTS_FOR_PICK;
+      score = score + getPointsForGame(result.name);
 
       if (games) {
         score = score + POINTS_FOR_GAMES;
