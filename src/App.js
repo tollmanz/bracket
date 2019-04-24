84aae5bd21ff3641a57cc33979b6d1576a6ada2c
Zack Tollman <tollmanz@gmail.com>
2019-04-23 20:49:00 -0500

    SPACE NOT TABS

--- src/App.js
+++ src/App.js
@@ -12,25 +12,25 @@ const linkStyle = {
 class App extends Component {
   render() {
     return (
-	  <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Blame Hockey</h1>
-		  <nav>
-		  	<Link style={linkStyle} to="/">Current Year</Link>
-			{ YEARS.map( y => {
-				if ( y === YEAR ) {
-					return '';
-				}
-				const yearString = "/y/" + y;
-				return <Link to={yearString} key={y} > {y} </Link> 
-			}) }
-		  </nav>
-		  <Route exact path="/" component={Score} />
-		  <Route path="/y/:year" component={Score} />
-        </header>
-      </div>
-	  </Router>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Blame Hockey</h1>
+            <nav>
+              <Link style={linkStyle} to="/">Current Year</Link>
+              {YEARS.map(y => {
+                if (y === YEAR) {
+                  return '';
+                }
+                const yearString = "/y/" + y;
+                return <Link to={yearString} key={y} > {y} </Link>
+              })}
+            </nav>
+            <Route exact path="/" component={Score} />
+            <Route path="/y/:year" component={Score} />
+          </header>
+        </div>
+      </Router>
     );
   }
 }
