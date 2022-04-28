02fdc82b7d85da89287da577ba526d50af134dc8
Zack Tollman <tollmanz@gmail.com>
2022-04-27 21:47:43 -0500

    Restore deleted files

---
+++ src/config/2019/picks.hockey.js
@@ -0,0 +1,43 @@
+module.exports = `--round 1--
+@thoronas: NSH 7, STL 6, CGY 6, VGK 7, TBL 5, BOS 7, WSH 7, PIT 6
+@vegasgeek: DAL 6, STL 6, CGY 5, VGK 6, TBL 5, BOS 6, WSH 7, NYI 6
+@desrosj: DAL 7, STL 7, CGY 6, VGK 7, TBL 5, BOS 5, WSH 6, PIT 5
+@aaronjorbin: NSH 5, STL 7, CGY 4, VGK 6, TBL 4, TOR 6, WSH 6, PIT 7
+@TTollman: NSH 5, STL 6, CGY 5, SJS 7, TBL 5, TOR 6, WSH 6, PIT 5
+@tollmanz: NSH 6, STL 5, CGY 4, VGK 6, TBL 4, BOS 7, WSH 5, NYI 7
+@nacin: TBL 5, TOR 7, WSH 5, PIT 7, NSH 6, STL 7, CGY 5, VGK 7
+@brothernacin: NSH 7, WPG 6, CGY 6, SJS 7, TBL 5, BOS 6, WSH 5, PIT 6
+@fathernacin: NSH 5, WPG 7, CGY 4, SJS 7, TBL 5, TOR 6, WSH 5, NYI 7
+
+--round 2--
+@thoronas: DAL 7, COL 6, CBJ 5, NYI 6
+@vegasgeek: DAL 5, SJS 7, CBJ 7, NYI 6
+@desrosj: DAL 7, SJS 5, BOS 6, NYI 6
+@aaronjorbin: STL 7, SJS 7, CBJ 7, CAR 7
+@TTollman: STL 6, SJS 6, CBJ 6, NYI 6
+@tollmanz: STL 5, SJS 6, BOS 7, NYI 6
+@nacin: STL 7, SJS 6, BOS 6, NYI 6
+@brothernacin: STL 6, SJS 5, BOS 7, NYI 6
+@fathernacin: STL 6, SJS 7, CBJ 6, NYI 7
+
+--round 3--
+@thoronas: BOS 6, SJS 7
+@vegasgeek: CAR 6, STL 6
+@desrosj: BOS 6, STL 7
+@aaronjorbin: BOS 4, STL 7
+@TTollman: STL 6, BOS 5
+@tollmanz: STL 6, BOS 7
+@nacin: BOS 6, SJS 7
+@brothernacin: BOS 7, SJS 6
+@fathernacin: CAR 6, SJS 7
+
+--round 4--
+@thoronas: BOS 6
+@vegasgeek: STL 6
+@desrosj: BOS 5
+@aaronjorbin: BOS 7
+@TTollman: STL 6
+@tollmanz: BOS 6
+@nacin: STL 7
+@brothernacin: STL 5
+@fathernacin: STL 6`;
