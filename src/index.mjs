1a6b9826fa4bc0ea78d3ea69e3e279db07a56a3d
Zack Tollman <tollmanz@gmail.com>
2022-04-28 00:22:22 -0500

    Lint all files

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.mjs';
-import { unregister } from './registerServiceWorker.mjs';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.mjs";
+import { unregister } from "./registerServiceWorker.mjs";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 unregister();
